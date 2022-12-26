9ac0388a42c13b7ac008991e1285c7a810355382
Doğukan <51451598+LewisVR@users.noreply.github.com>
2022-12-27 01:25:14 +0300

    Create hesapla.py

---
+++ hesapla.py
@@ -0,0 +1,5 @@
+sinav1= input("2. Yazılı Notunuzu Giriniz: ")
+sinav2 = input("2. Yazılı Notunuzu Giriniz: ")
+
+ortalama = (float(sınav1)+float(sınav2))/3
+print("Ortalama Notunuz: ", format(ortalama))
